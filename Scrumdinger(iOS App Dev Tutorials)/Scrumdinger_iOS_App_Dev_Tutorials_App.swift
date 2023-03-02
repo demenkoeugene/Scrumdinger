6cc7bc2d3c41b6efc71a2c9bf98a3c51923a8bfe
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-03-02 14:03:54 +0200

    Initial Commit

---
+++ Scrumdinger(iOS App Dev Tutorials)/Scrumdinger_iOS_App_Dev_Tutorials_App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_iOS_App_Dev_Tutorials_App.swift
+//  Scrumdinger(iOS App Dev Tutorials)
+//
+//  Created by Eugene Demenko on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_iOS_App_Dev_Tutorials_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
